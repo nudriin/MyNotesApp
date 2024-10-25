82b4668ad0ab23c6177656d57720d2fdc789da27
Nurdin <mrsunnysummer@gmail.com>
2024-10-25 17:19:35 +0700

    feat: implement in main activity

--- app/src/main/java/com/nudriin/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/nudriin/mynotesapp/MainActivity.kt
@@ -1,20 +1,126 @@
 package com.nudriin.mynotesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.nudriin.mynotesapp.databinding.ActivityMainBinding
+import com.nudriin.mynotesapp.db.NoteHelper
+import com.nudriin.mynotesapp.helper.MappingHelper
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: NoteAdapter
+
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
+
+    val resultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if(result.data != null) {
+            when(result.resultCode) {
+                NoteAddUpdateActivity.RESULT_ADD -> {
+                    val note = result.data?.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                    adapter.addItem(note)
+                    binding.rvNotes.smoothScrollToPosition(adapter.itemCount - 1)
+                    showSnackbarMessage("Satu item berhasil ditambahkan")
+                }
+                NoteAddUpdateActivity.RESULT_UPDATE -> {
+                    val note = result.data?.getParcelableExtra<Note>(NoteAddUpdateActivity.EXTRA_NOTE) as Note
+                    val position = result?.data?.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0) as Int
+                    adapter.updateItem(position, note)
+                    binding.rvNotes.smoothScrollToPosition(position)
+                    showSnackbarMessage("Satu item berhasil diubah")
+                }
+                NoteAddUpdateActivity.RESULT_DELETE -> {
+                    val position = result?.data?.getIntExtra(NoteAddUpdateActivity.EXTRA_POSITION, 0) as Int
+                    adapter.deleteItem(position)
+                    showSnackbarMessage("Satu item berhasil dihapus")
+                }
+
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Notes"
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+
+        adapter = NoteAdapter(object : NoteAdapter.OnItemClickCallback {
+            override fun onItemClicked(selectedNote: Note?, position: Int?) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, selectedNote)
+                intent.putExtra(NoteAddUpdateActivity.EXTRA_POSITION, position)
+                resultLauncher.launch(intent)
+            }
+        })
+
+        binding.rvNotes.adapter = adapter
+
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+        if (savedInstanceState == null) {
+            // proses ambil data
+            loadNotesAsync()
+        } else {
+            val list = savedInstanceState.getParcelableArrayList<Note>(EXTRA_STATE)
+            if (list != null) {
+                adapter.listNotes = list
+            }
+        }
+    }
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.rvNotes, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapter.listNotes)
+    }
+
+    private fun loadNotesAsync() {
+        lifecycleScope.launch {
+            binding.progressbar.visibility = View.VISIBLE
+            val noteHelper = NoteHelper.getInstance(applicationContext)
+            noteHelper.open()
+
+            val deferredNotes = async(Dispatchers.IO) {
+                val cursor = noteHelper.queryAll()
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+
+            binding.progressbar.visibility = View.GONE
+
+            val notes = deferredNotes.await()
+
+            if(notes.size > 0) {
+                adapter.listNotes = notes
+            } else {
+                adapter.listNotes = ArrayList()
+                showSnackbarMessage("Tidak ada data saat ini")
+            }
+            noteHelper.close()
         }
     }
 }
